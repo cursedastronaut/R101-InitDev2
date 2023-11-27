f4c1e82a2aff6d72d61e3c43d009c2c6e7e462fe
cursedastronaut <g.martineaux@student.isartdigital.com>
2023-11-27 11:25:37 +0100

    TP2: Ajout de la Partie 2

--- TP2/appartement.cpp
+++ TP2/appartement.cpp
@@ -1,5 +1,6 @@
 #include "appartement.h"
 
+//Transforme une chaine de caractère en int strictement positif.
 int toInt(const char* index) {
 	std::string temp = index;
 	if (temp.size() == 0)
@@ -15,6 +16,16 @@ int toInt(const char* index) {
 	}
 	return result;
 }
+
+uint32_t Piece::surface() {
+	return taille.largeur * taille.longueur;
+}
+
+/*
+-----------------------------------------------
+	CLASSE:	PIECE
+-----------------------------------------------
+*/
 
 //Constructeur vide
 Piece::Piece() {
@@ -91,9 +102,7 @@ void Piece::saisir() {
 	
 }
 
-uint32_t Piece::surface() {
-	return taille.largeur * taille.longueur;
-}
+
 
 bool Piece::plusGrande(Piece &autre) {
 	return (surface() > autre.surface());
@@ -105,4 +114,76 @@ uint2 Piece::obtenirTaille() {
 
 uint32_t surface(Piece &piece) {
 	return piece.obtenirTaille().largeur * piece.obtenirTaille().longueur;
+}
+
+/*
+-----------------------------------------------
+	CLASSE:	APPARTEMENT
+-----------------------------------------------
+*/
+
+Appartement::Appartement() {
+	adresse = "1 rue Joliot-Curie 91400 Orsay";
+}
+
+Appartement::Appartement(Piece &piece, string adresseFournie) {
+	adresse = adresseFournie;
+	pieces.push_back(piece);
+}
+
+void Appartement::afficher() {
+	cout	<< endl << "Appartement a l'adresse : " << adresse << endl;
+	for (size_t i = 0; i < pieces.size(); ++i)
+		pieces[i].afficher(); 
+}
+
+void Appartement::ajoutPiece(Piece &pieceFournie) {
+	pieces.push_back(pieceFournie);
+}
+
+Piece Appartement::retraitPiece(size_t indice) {
+	Piece temp;
+	if (indice >= pieces.size())
+		throw invalid_argument("Indice superieur au nombre de pieces");
+	
+	temp = pieces[indice];
+	if (pieces.size() != 1) 
+		pieces[indice] = pieces[pieces.size()-1];
+	pieces.pop_back();
+}
+
+uint32_t Appartement::nbPieces() {
+	return pieces.size();
+}
+
+uint32_t Appartement::surface() {
+	uint32_t temp = 0;
+	for (size_t i = 0; i < pieces.size(); ++i)
+		temp += pieces[i].surface();
+	return temp;
+}
+
+int8_t Appartement::compare(Appartement& autre) {
+	//Permets de ne pas rappeler les fonctions à chaque tests.
+	uint32_t maSurface = surface(), autreSurface = autre.surface();
+
+	if (maSurface < autreSurface)
+		return -1;
+	else if (maSurface == autreSurface)
+		return 0;
+	else //if (maSurface > autreSurface), ça n'a pas d'intérêt de refaire le test.
+		return 1;
+}
+
+int Appartement::plusGrandePiece() {
+	if (pieces.size() == 0)
+		return -1;
+	
+	size_t plusGrandeSurface = 0;
+	for (size_t i = 0; i < pieces.size(); ++i) {
+		if (pieces[i].surface() > pieces[plusGrandeSurface].surface())
+			plusGrandeSurface = i;
+	}
+
+	return (int)plusGrandeSurface;
 }
--- TP2/appartement.h
+++ TP2/appartement.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <limits>
 #include <cmath>
+#include <vector>
 using namespace std;
 
 
@@ -36,3 +37,33 @@ class Piece {
 };
 
 uint32_t surface(Piece &piece);
+
+class Appartement {
+	public:
+		//Constructeur vide
+		Appartement();
+		//Constructeur qui construit un appartement avec une seule pièce passée en paramètre, et une adresse passée en paramètre.
+		Appartement(Piece &piece, string adresse);
+		//Affiche les différentes pièces d'un appartement.
+		void afficher();
+		//Ajoute une pièce passée en paramètres.
+		void ajoutPiece(Piece &pieceFournie);
+		//Supprime la pièce à l'indice fourni, et renvoie une copie.
+		Piece retraitPiece(size_t indice);
+		//Renvoie le nombre de pièces d'un appartement
+		uint32_t nbPieces();
+		//Renvoie la surface totale d'un appartement
+		uint32_t surface();
+		//Renvoie -1 si l'appartement cible a une surface plus petite.
+		//Renvoie  0 si l'appartement cible a une surface égale.
+		//Renvoie  1 si l'appartement cible a une surface plus grande.
+		int8_t compare(Appartement& autre);
+		//Renvoie l'indice de la pièce de plus grande surface (-1 si aucune Piece dans l'appartement).
+		int plusGrandePiece();
+
+	private:
+		//Adresse de l'appartement, pas de norme particulière
+		string adresse;
+		//Toutes les pièces d'un appartement.
+		vector<Piece> pieces;
+};
