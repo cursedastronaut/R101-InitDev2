222b5e918ebe4a4a87f7cc06e67d61f414b0381f
cursedastronaut <g.martineaux@student.isartdigital.com>
2024-01-09 17:26:42 +0100

    TP6: Ajout d'un enum et de commentaires

--- TP6/main.cpp
+++ TP6/main.cpp
@@ -1,15 +1,15 @@
 #include "magasin.hpp"
 void testAuto(uint8_t partie);
-
+enum parties { PART1, PART2, PART3};
 int main(int argc, char* argv[]) {
 	using std::cout, std::endl;
 	srand(time(0));
 
 	//Partie 1
-	testAuto(0);
+	testAuto(PART1);
 
 	//Partie 2
-	testAuto(1);
+	testAuto(PART2);
 	{
 		Chocolat last;
 		BoiteChoco boite;
@@ -23,8 +23,6 @@ int main(int argc, char* argv[]) {
 		cout << endl;
 	}
 
-
-
 	return 0;
 }
 
@@ -32,28 +30,35 @@ void testAuto(uint8_t partie) {
 	using std::cout, std::endl;
 
 	//I won't use a switch for clarity purposes.
-	if (partie == 0) {
+	//Partie 1
+	if (partie == PART1) {
 		std::vector<Chocolat> chocolats;
 		chocolats.push_back(Chocolat("blanc",20));    // chocolat blanc de 20g
 		chocolats.push_back(Chocolat());  // chocolat noir de 30g
 		chocolats.push_back(Chocolat(10));  // chocolat de 10g, nature tirée au sort
 		chocolats.push_back(Chocolat("blanc") ); // chocolat blanc de 30g
+		
+		//Test de Chocolat::lePoids
 		if (chocolats[0].lePoids() == 20 && (chocolats[1].lePoids() == chocolats[3].lePoids() && chocolats[3].lePoids() == POIDS) && chocolats[2].lePoids() == 10)
 			cout << "Test de Chocolat::lePoids passe." << endl;
 		else
 			cout << "Test de Chocolat::lePoids NON PASSE." << endl;
 
+		//Test de Chocolat::laNature
 		if (chocolats[0].laNature() == "blanc" && chocolats[1].laNature() == "noir" && chocolats[3].laNature() == "blanc")
 			cout << "Test de Chocolat::laNature passe." << endl;
 		else
 			cout << "Test de Chocolat::laNature NON PASSE." << endl;
 
+		//Test de Chocolat::operator<
 		if (chocolats[2] < chocolats[0] && chocolats[0] < chocolats[1])
 			cout << "Test de Chocolat::operator< passe." << endl;
 		else
 			cout << "Test de Chocolat::operator< NON PASSE." << endl;
-	} else if (partie == 1) {
+	//Partie 2
+	} else if (partie == PART2) {
 		{
+			//Test de BoiteChoco::BoiteChoco() et BoiteChoco::leNom
 			BoiteChoco boite;
 			if (boite.leNom() == CLASSIQUE) {
 				cout << "Test de BoiteChoco::BoiteChoco() passe" << endl;
@@ -63,6 +68,8 @@ void testAuto(uint8_t partie) {
 				cout << "Test de BoiteChoco::leNom() NON PASSE" << endl;
 				cout << "Test de BoiteChoco::BoiteChoco() NON PASSE" << endl;
 			}
+
+			//Test de BoiteChoco::LePoids() et BoiteChoco::ajoutChocolat
 			boite.ajoutChocolat(Chocolat(5));
 			if (boite.lePoids() == 5) {
 				cout << "Test de BoiteChoco::LePoids() passe." << endl;
@@ -73,13 +80,14 @@ void testAuto(uint8_t partie) {
 			} 
 		}
 		{
+			//Test de BoiteChoco::BoiteChoco(...)
 			BoiteChoco boite("Pimprenelle", 7);
 			if (boite.leNom() == "Pimprenelle" && boite.lePoids() == 7*POIDS_BOITE)
 				cout << "Test de BoiteChoco::BoiteChoco(...) passe" << endl;
 			else
 				cout << "Test de BoiteChoco::BoiteChoco(...) NON PASSE" << endl;
 		}
-	} else if (partie == 2) {
+	} else if (partie == PART3) {
 
 	}
 }
